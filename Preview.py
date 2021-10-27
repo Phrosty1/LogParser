f7af85b954172f716184376f2395bd6d4d85e91d
Phrosty1 <istas.phrost@gmail.com>
2021-10-27 11:42:22 -0400

    Update Preview.py

--- Preview.py
+++ Preview.py
@@ -9,16 +9,17 @@ import time # time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(1634309423832/10
 # https://pandas.pydata.org/docs/reference/api/pandas.merge_asof.html
 folderEventSpecs = os.path.realpath('../ESOUIDocumentation/')
 folderRawLogs = os.path.realpath('../AnyLoggerRawLuaDumps/')
-folderEventData = os.path.realpath('../Sandbox_Brian/EventData/') # ; folderEventData = os.path.realpath('C:\FolderToPutLogFiles\EsoAnalytics\Sandbox_Brian\EventData')
+folderEventData = os.path.realpath('../Sandbox_Brian/EventData/') ; folderEventData = os.path.realpath('C:\FolderToPutLogFiles\EsoAnalytics\Sandbox_Brian\EventData')
 folderSandBox = os.path.realpath('../Sandbox_Brian/')
 def getEventDataframe(event='EVENT_COMBAT_EVENT', days=0):
    if os.path.exists(folderEventData+'/'+event) :
-      return pd.concat(pd.read_parquet(folderEventData+'/'+event+'/'+f) for f in ([f for f in os.listdir(folderEventData+'/'+event) if f.endswith('.parquet')][-days:]))
+      if days==0: return pd.read_parquet(folderEventData+'/'+event)
+      else: return pd.concat(pd.read_parquet(folderEventData+'/'+event+'/'+f) for f in ([f for f in os.listdir(folderEventData+'/'+event) if f.endswith('.parquet')][-days:]))
 def resortIndex(eventsDataframe):
    eventsDataframe.sort_values( ['timestamp', 'player', 'seq'], ignore_index=True, inplace=True)
    return eventsDataframe
 
-EVENT_COMBAT_EVENT = getEventDataframe('EVENT_COMBAT_EVENT', 1)
+EVENT_COMBAT_EVENT = getEventDataframe('EVENT_COMBAT_EVENT')
 # display(EVENT_COMBAT_EVENT)
 EVENT_PLAYER_COMBAT_STATE = resortIndex(getEventDataframe('EVENT_PLAYER_COMBAT_STATE'))
 # display(EVENT_PLAYER_COMBAT_STATE)
@@ -30,7 +31,7 @@ CALC_TIMES_IN_COMBAT = pd.DataFrame(pd.merge_asof(EVENT_PLAYER_COMBAT_STATE[EVEN
          allow_exact_matches=False)
    , columns=['player','timestamp_left','timestamp_right']).rename(columns={'player':'player', 'timestamp_left':'combat_begin', 'timestamp_right':'combat_end'})
 CALC_TIMES_IN_COMBAT['duration'] = CALC_TIMES_IN_COMBAT['combat_end']-CALC_TIMES_IN_COMBAT['combat_begin']
-# display(CALC_TIMES_IN_COMBAT.sort_values( ['duration']))
+display(CALC_TIMES_IN_COMBAT.sort_values( ['duration']))
 
 # ALL = resortIndex(getEventDataframe('ALL'))
 # display(ALL[ALL['timestamp'].between(1634415375075, 1634415920805)])
@@ -64,4 +65,37 @@ report.to_html(folderSandBox+'/'+'output.html')
 
 # bigbattle = getEventDataframe('ALL')
 
+bigbattle = pd.DataFrame(EVENT_COMBAT_EVENT[EVENT_COMBAT_EVENT['timestamp'].between(1634415375075, 1634415920805)])
+# bigbattle = bigbattle[bigbattle['result'].isin(['BEGIN','CRITICAL_DAMAGE','DAMAGE','DOT_TICK','DOT_TICK_CRITICAL'])]
+EVENT_UNIT_DEATH_STATE_CHANGED = getEventDataframe('EVENT_UNIT_DEATH_STATE_CHANGED')
+
+# What Ability keeps killing us
+# How much is this skill benefiting another?
+# ALL = resortIndex(getEventDataframe('ALL'))
+# tmp1 = ALL[ALL['timestamp'].between(1634949919302, 1634949919359)]
+# display(tmp1)
+# tmp2 = pd.DataFrame(EVENT_COMBAT_EVENT[EVENT_COMBAT_EVENT['timestamp'].between(1634949919302, 1634949919359)])
+# display(tmp2)
+# tmp1.to_html(folderSandBox+'/'+'output.html')
+
+# display(EVENT_UNIT_DEATH_STATE_CHANGED[EVENT_UNIT_DEATH_STATE_CHANGED['isDead']=='false'])
+# display(EVENT_UNIT_DEATH_STATE_CHANGED[EVENT_UNIT_DEATH_STATE_CHANGED['unitTag']=='player'])
+
+def cleanName(name): return name.split('^')[0]
+EVENT_COMBAT_EVENT['sourceName'] = EVENT_COMBAT_EVENT['sourceName'].apply(cleanName)
+EVENT_COMBAT_EVENT['targetName'] = EVENT_COMBAT_EVENT['targetName'].apply(cleanName)
+combatHits = pd.DataFrame(EVENT_COMBAT_EVENT[EVENT_COMBAT_EVENT['result'].isin(['BEGIN','CRITICAL_DAMAGE','DAMAGE','DOT_TICK','DOT_TICK_CRITICAL'])]).sort_values( ['result', 'abilityId', 'abilityName', 'timestamp', 'seq'], ignore_index=False)
+combatHits.dropna(subset=['sourceName'], inplace=True)
+
+
+display(combatHits['sourceName'])
+# combatHits['sourceName'].apply(lambda x : x.split('^')[0])
+# combatHits['sourceName'] = combatHits['sourceName'].apply(lambda x : x.split('^')[0])
+# display(combatHits)
+display(combatHits[combatHits['player'] == combatHits['sourceName']])
+# display(cleanName('Hadara Hazelwood^Fx'))
+# report = pd.DataFrame(combatHits.groupby(by=['sourceName','abilityName'], observed=True)['hitValue'].describe()) # describe / mean
+# report.sort_values( ['hitValue','abilityName'], ignore_index=False, inplace=True)
+display(report.sort_values('mean'))
+# report.to_html(folderSandBox+'/'+'output.html')
 
