d97c152c86d82ef60640e61f6fa2b7f22c7b486f
Phrosty1 <istas.phrost@gmail.com>
2021-10-27 12:42:44 -0400

    Update Preview.py

--- Preview.py
+++ Preview.py
@@ -18,12 +18,11 @@ def getEventDataframe(event='EVENT_COMBAT_EVENT', days=0):
 def resortIndex(eventsDataframe):
    eventsDataframe.sort_values( ['timestamp', 'player', 'seq'], ignore_index=True, inplace=True)
    return eventsDataframe
+def cleanName(name): return name.split('^')[0]
 
 EVENT_COMBAT_EVENT = getEventDataframe('EVENT_COMBAT_EVENT')
-# display(EVENT_COMBAT_EVENT)
+EVENT_POWER_UPDATE = resortIndex(getEventDataframe('EVENT_POWER_UPDATE')) # [14909] = "1635293485592;Nissa Forbus;EVENT_POWER_UPDATE;boss1;999;-2;923059;923059;923059",
 EVENT_PLAYER_COMBAT_STATE = resortIndex(getEventDataframe('EVENT_PLAYER_COMBAT_STATE'))
-# display(EVENT_PLAYER_COMBAT_STATE)
-
 CALC_TIMES_IN_COMBAT = pd.DataFrame(pd.merge_asof(EVENT_PLAYER_COMBAT_STATE[EVENT_PLAYER_COMBAT_STATE.inCombat=="true"], EVENT_PLAYER_COMBAT_STATE[EVENT_PLAYER_COMBAT_STATE.inCombat=="false"],
          by='player', direction='forward', # in ["backward", "forward", "nearest"]
          suffixes=['_left','_right'],
@@ -32,8 +31,9 @@ CALC_TIMES_IN_COMBAT = pd.DataFrame(pd.merge_asof(EVENT_PLAYER_COMBAT_STATE[EVEN
    , columns=['player','timestamp_left','timestamp_right']).rename(columns={'player':'player', 'timestamp_left':'combat_begin', 'timestamp_right':'combat_end'})
 CALC_TIMES_IN_COMBAT['duration'] = CALC_TIMES_IN_COMBAT['combat_end']-CALC_TIMES_IN_COMBAT['combat_begin']
 display(CALC_TIMES_IN_COMBAT.sort_values( ['duration']))
-
 # ALL = resortIndex(getEventDataframe('ALL'))
+
+
 # display(ALL[ALL['timestamp'].between(1634415375075, 1634415920805)])
 # display(EVENT_COMBAT_EVENT[EVENT_COMBAT_EVENT['timestamp'].between(1634415375075, 1634415920805)])
 
@@ -50,10 +50,6 @@ report.to_html(folderSandBox+'/'+'output.html')
 # display(getEventDataframe(event='EVENT_COMBAT_EVENT').groupby('player').boxplot())
 # df = EVENT_PLAYER_COMBAT_STATE[EVENT_PLAYER_COMBAT_STATE['inCombat']=='true']
 # df = EVENT_PLAYER_COMBAT_STATE; df['Data_lagged'] = df.groupby(['player'])['timestamp'].shift(1)
-
-# trades = pd.DataFrame({ 'time': pd.to_datetime(['20160525 13:30:00.023', '20160525 13:30:00.038', '20160525 13:30:00.048', '20160525 13:30:00.048', '20160525 13:30:00.048']), 'ticker': ['MSFT', 'MSFT', 'GOOG', 'GOOG', 'AAPL'], 'price': [51.95, 51.95, 720.77, 720.92, 98.00], 'quantity': [75, 155, 100, 100, 100]}, columns=['time', 'ticker', 'price', 'quantity'])
-# quotes = pd.DataFrame({ 'time': pd.to_datetime(['20160525 13:30:00.023', '20160525 13:30:00.023', '20160525 13:30:00.030', '20160525 13:30:00.041', '20160525 13:30:00.048', '20160525 13:30:00.049', '20160525 13:30:00.072', '20160525 13:30:00.075']), 'ticker': ['GOOG', 'MSFT', 'MSFT', 'MSFT', 'GOOG', 'AAPL', 'GOOG', 'MSFT'], 'bid': [720.50, 51.95, 51.97, 51.99, 720.50, 97.99, 720.50, 52.01], 'ask': [720.93, 51.96, 51.98, 52.00, 720.93, 98.01, 720.88, 52.03]}, columns=['time', 'ticker', 'bid', 'ask'])
-# pd.merge_asof(trades, quotes, on='time', by='ticker')
 
 # df = pd.merge_asof(EVENT_PLAYER_COMBAT_STATE, EVENT_PLAYER_COMBAT_STATE, on='timestamp', by='player', allow_exact_matches=False)
 # display(df)
@@ -81,7 +77,6 @@ EVENT_UNIT_DEATH_STATE_CHANGED = getEventDataframe('EVENT_UNIT_DEATH_STATE_CHANG
 # display(EVENT_UNIT_DEATH_STATE_CHANGED[EVENT_UNIT_DEATH_STATE_CHANGED['isDead']=='false'])
 # display(EVENT_UNIT_DEATH_STATE_CHANGED[EVENT_UNIT_DEATH_STATE_CHANGED['unitTag']=='player'])
 
-def cleanName(name): return name.split('^')[0]
 EVENT_COMBAT_EVENT['sourceName'] = EVENT_COMBAT_EVENT['sourceName'].apply(cleanName)
 EVENT_COMBAT_EVENT['targetName'] = EVENT_COMBAT_EVENT['targetName'].apply(cleanName)
 combatHits = pd.DataFrame(EVENT_COMBAT_EVENT[EVENT_COMBAT_EVENT['result'].isin(['BEGIN','CRITICAL_DAMAGE','DAMAGE','DOT_TICK','DOT_TICK_CRITICAL'])]).sort_values( ['result', 'abilityId', 'abilityName', 'timestamp', 'seq'], ignore_index=False)
